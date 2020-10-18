e32567d75711daf276fa201ed8e0fcdae0a18ced
José Luiz <joseluizgnogueira@live.com>
2020-10-18 17:28:14 -0300

    added integrate function

--- src/matlab/RawData/funcs/calculate_gyro_relative_tilt.m
+++ src/matlab/RawData/funcs/calculate_gyro_relative_tilt.m
@@ -1,7 +1,8 @@
 function ret = calculate_gyro_relative_tilt(old_tilt, G, old_G, freq_sample)
-    newRoll  = (old_tilt(1) + ((G(1) + old_G(1)) / (2 * freq_sample)));
-    newPitch = (old_tilt(2) + ((G(2) + old_G(2)) / (2 * freq_sample)));
-    newYaw   = (old_tilt(3) + ((G(3) + old_G(3)) / (2 * freq_sample)));
+    period = 1/freq_sample;
+    newRoll  = integrate(old_tilt(1), G(1), old_G(1), period);
+    newPitch = integrate(old_tilt(2), G(2), old_G(2), period);
+    newYaw   = integrate(old_tilt(3), G(3), old_G(3), period);
 
     ret = [newRoll, newPitch, newYaw];
 end
--- src/matlab/RawData/funcs/calculate_position.m
+++ src/matlab/RawData/funcs/calculate_position.m
@@ -1,7 +1,8 @@
 function ret = calculate_position(old_value, velocity, old_velocity, freq_sample)
-    newPx = (old_value(1) + ((velocity(1) + old_velocity(1)) / (2 * freq_sample) ));
-    newPy = (old_value(2) + ((velocity(2) + old_velocity(2)) / (2 * freq_sample) ));
-    newPz = (old_value(3) + ((velocity(3) + old_velocity(3)) / (2 * freq_sample) ));
+    period = 1/freq_sample;
+    newPx = integrate(old_value(1), velocity(1), old_velocity(1), period);
+    newPy = integrate(old_value(2), velocity(2), old_velocity(2), period);
+    newPz = integrate(old_value(3), velocity(3), old_velocity(3), period);
 
     ret = [newPx, newPy, newPz];
 end
