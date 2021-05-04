44879cd031a76c7dbb70c062b0468e599b7796cd
José Luiz <joseluizgnogueira@live.com>
2021-05-04 10:19:07 -0300

    fix for acel mag tilt: normalize acel

--- src/matlab/RawData/charts/acel_mag_tilt/calculate_acel_mag_tilt.m
+++ src/matlab/RawData/charts/acel_mag_tilt/calculate_acel_mag_tilt.m
@@ -1,20 +1,18 @@
 function ret = calculate_acel_mag_tilt(A, H)
+    normA = A / norm(A);
+
     %% Calcula pich e roll usado aceleração
-    newPitch = atan2(-A(1), sqrt( A(2)^2 + A(3)^2 )) * 180/pi;
-    if (A(3)>=0)
-        sign = 1;
-    else
-        sign = -1;
-    end
-    miu = 0.001;
-    newRoll = atan2( A(2),   (sign * sqrt( A(3)^2 + miu * A(1)^2 ))) * 180/pi;
+    % O uso da função real() aqui é para obter a parte real do número
+    % por algum motivo o matlab estava retornando um número complexo para o caso onde normA(1) = 1.000...
+    newPitch = real(asin(-normA(1))) * 180/pi;
+    newRoll = atan2(normA(2), normA(3)) * 180/pi;
 
     %% Calcula yaw usando magnetômetro (compass com compensação)
     % Ref do calculo: https://www.mikrocontroller.net/attachment/292888/AN4248.pdf
     x = H(1)*cosd(newPitch) + H(2)*sind(newRoll)*sind(newPitch) + H(3)*cosd(newRoll)*sind(newPitch);
-    y = -H(2)*cosd(newRoll) + H(3)*sind(newRoll);
+    y = H(2)*cosd(newRoll) - H(3)*sind(newRoll);
 
-    newYaw = -atan2(-y, x) * 180/pi;
+    newYaw = atan2(-y, x) * 180/pi;
 
     ret = [newRoll, newPitch, newYaw];
 end
